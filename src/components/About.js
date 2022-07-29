d5a19340c0c1f389b4764e1c9ac78ddf79858d0a
partho2589 <parthomalo615@.com>
2022-07-29 21:17:23 +0600

    added icon and imeage

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1 className='text-secondary'>This is about your doctors.</h1>
+        </div>
+    );
+};
+
+export default About;
